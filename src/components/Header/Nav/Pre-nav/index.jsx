6d4499f002f82f621f307c8791d411355cbd6886
ishan shahzad <3006761319ishan@gmail.com>
2020-03-24 13:25:19 +0500

    initial commit

---
+++ src/components/Header/Nav/Pre-nav/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PreNav = () => {
+  return (
+    <div className="prenav">
+      <ul>
+        <li>Marketplace</li>
+        <li>Help & FAQs</li>
+        <li>location</li>
+      </ul>
+    </div>
+  );
+};
+
+export default PreNav;
